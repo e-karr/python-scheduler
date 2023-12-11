46dc241f3bd096ef951e16f028e56c4a0d695a21
Elizabeth Karr <e.karr93@icloud.com>
2023-12-10 18:57:10 -0600

    tried different way to make schedule

--- scheduler.py
+++ scheduler.py
@@ -15,26 +15,54 @@ teams_dataframe = pd.read_csv(file)
 # create round robin schedule, keep number of weeks requested, and return dataframe
 # TODO figure out how to do this without having to create a round robin schedule first
 # TODO if uneven number of teams, give one team a bye each week
-def create_schedule(teams, weeks):
-    first_row = random.sample(teams, len(teams))
-    permutes = random.sample(range(len(teams)), len(teams))
+# def create_schedule(teams, weeks):
+#     first_row = random.sample(teams, len(teams))
+#     permutes = random.sample(range(len(teams)), len(teams))
     
-    schedule = [first_row[i:] + first_row[:i] for i in permutes]
+#     schedule = [first_row[i:] + first_row[:i] for i in permutes]
 
-    schedule_df = pd.DataFrame(schedule)
-    schedule_df.columns = ['team_name'] + list(schedule_df.columns[1:])
-    schedule_df = schedule_df.iloc[:, :weeks + 1]
+#     schedule_df = pd.DataFrame(schedule)
+#     schedule_df.columns = ['team_name'] + list(schedule_df.columns[1:])
+#     schedule_df = schedule_df.iloc[:, :weeks + 1]
     
-    return schedule_df
+#     return schedule_df
+
+def create_schedule(teams, weeks):
+    schedule = []
+    for team in teams:
+        team_schedule = []
+        team_schedule.append(team)
+
+        for i in range(weeks):
+            opponent = random.choice(teams)
+            max_attempts = 10
+            attempts = 0
+            while (
+                opponent in team_schedule or 
+                any(opponent == sublist[i + 1] for sublist in schedule)
+            ):
+                attempts += 1
+                if attempts >= max_attempts:
+                    create_schedule(teams, weeks)
+                opponent = random.choice(teams)
+
+            team_schedule.append(opponent)
+    
+        schedule.append(team_schedule)
+
+    schedule = pd.DataFrame(schedule)
+    schedule.columns = ['team_name'] + list(schedule.columns[1:])
+
+    return schedule
 
 schedule = create_schedule(teams_dataframe["team_name"].tolist(), schedule_length)
 
 final_schedule = teams_dataframe.merge(schedule, how='inner', on='team_name')
 
-filepath = Path('./output/schedule.csv')
-filepath.parent.mkdir(parents=True, exist_ok=True)
+# filepath = Path('./output/schedule.csv')
+# filepath.parent.mkdir(parents=True, exist_ok=True)
 
-final_schedule.to_csv(filepath)
+# final_schedule.to_csv(filepath)
 
 print(final_schedule)
 
